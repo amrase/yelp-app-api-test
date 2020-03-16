50ad36c6d49d0ac31e8886c4543365fb67419e6a
Amra Sezairi <amra.sezairi@hotmail.com>
2020-03-16 03:04:59 -0400

    Added SearchBar

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {View,Text,StyleSheet} from 'react-native'
+import SearchBar from '../components/SearchBar'
 
 const SearchScreen = () =>{
+    const [term,setTerm] = useState('')
+
+    console.log(term)
+
     return <View>
-        <Text>SearchScreen</Text>
+        <SearchBar
+             term={term} 
+             onTermChange={(newTerm)=>setTerm(newTerm)}
+             onTermSubmit={()=>console.log('On term submit!')}
+         />
+        <Text>Search Screen</Text>
+        <Text>{term}</Text>
     </View>
 }
 
