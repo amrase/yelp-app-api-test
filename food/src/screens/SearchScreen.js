b67012c6bb65f61e06807a53cde3694845e6e883
Amra Sezairi <amra.sezairi@hotmail.com>
2020-03-16 20:44:27 -0400

    Error Handle

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import {View,Text,StyleSheet} from 'react-native'
 import SearchBar from '../components/SearchBar'
 import yelp from '../api/yelp'
@@ -8,15 +8,35 @@ const SearchScreen = () =>{
 
     const [term,setTerm] = useState('')
     const [results,setResults] = useState([]);
+    // const [errorMessage,setErrorMessage] = useState('')
 
 
+    // useEffect(() => {
+    //     searchApi()
+    // }, [])
 
+    //helper function
+    const searchApi = async () =>{
+        try {
+       const response = await yelp.get('/search',{
+           params: {
+                limit:50,
+                term,
+                location: 'New York'
+           }
+       });
+       setResults(response.data.businesses)
+        }
+        catch(e){
+            alert('Something went wrong')
+        }
+    }
 
     return <View>
         <SearchBar
              term={term} 
              onTermChange={(newTerm)=>setTerm(newTerm)}
-             onTermSubmit={()=>console.log('On term submit!')}
+             onTermSubmit={()=>searchApi()}
          />
         <Text>Search Screen</Text>
         <Text>We have found {results.length} results</Text>
